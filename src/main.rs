35b08f80bc27d356829dc510f8063c1aa0ecadf5
Timothy Simons <timothy.simons@oracle.com>
2023-07-08 16:33:51 +0200

    Finish draft write blob method

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
-mod blob;
-mod objectstore;
-mod tree;
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![warn(clippy::cargo)]
 
-fn main() {
-    println!("main")
+use std::env;
+use std::fs;
+use std::io;
+
+mod objectstore;
+
+fn main() -> io::Result<()> {
+    let cwd = env::current_dir()?;
+    let object_path = cwd.join("objects");
+    fs::create_dir_all(&object_path)?;
+    let objstore = objectstore::ObjectStore::new(object_path);
+    let filepath = cwd.join("somethingorother.txt");
+    objstore.write_blob(filepath, 8 * 1024)?;
+    Ok(())
 }
--- src/objectstore.rs
+++ src/objectstore.rs
@@ -11,46 +11,51 @@ use uuid::Uuid;
 
 const OBJ_DIR_LEN: usize = 2;
 
-struct ObjectStore {
+#[allow(clippy::redundant_pub_crate)]
+pub(crate) struct ObjectStore {
     location: path::PathBuf,
 }
 
 impl ObjectStore {
-    pub(crate) fn new(location: path::PathBuf) -> ObjectStore {
-        fs::create_dir(location);
-        ObjectStore { location }
+    pub(crate) const fn new(location: path::PathBuf) -> Self {
+        Self { location }
     }
 
-    pub(crate) fn write_blob(&self, file: fs::File, buffer_size: usize) -> io::Result<String> {
-        let uuid_filename = format!("{}", Uuid::new_v4());
-        let uuid_filepath = self.location.join(uuid_filename);
-        let temporary_file = fs::File::create(uuid_filepath)?;
+    pub(crate) fn write_blob(&self, file_path: path::PathBuf, buffer_size: usize) -> io::Result<String> {
+        let mut file = fs::File::open(file_path)?;
+        let uuid_file_name = format!("{}", Uuid::new_v4());
+        let uuid_file_path = self.location.join(&uuid_file_name);
+        let temporary_file = fs::File::create(&uuid_file_path)?;
 
-        let mut buf_reader = io::BufReader::with_capacity(buffer_size, file);
         let mut encoder = DeflateEncoder::new(temporary_file, Compression::default());
         let mut hasher = Sha256::new();
 
         let size = file.metadata()?.len();
-        let header = format!("blob {}\0", size);
-        encoder.write(header.as_bytes());
+        let header = format!("blob {size}\0");
+        encoder.write_all(header.as_bytes())?;
         hasher.update(header.as_bytes());
-        let mut buffer = Vec::new();
+
+        let mut buffer = vec![0; buffer_size];
         loop {
-            if buf_reader.read(&mut buffer)? == 0 {
+            let bytes_read = file.read(&mut buffer)?;
+            if bytes_read == 0 {
                 break;
             }
-            encoder.write(&buffer);
-            hasher.update(&buffer);
+            let buffer_slice = &buffer[..bytes_read];
+            encoder.write_all(buffer_slice)?;
+            hasher.update(buffer_slice);
         }
-        encoder.finish();
+        encoder.finish()?;
         let digest = hasher.finalize();
+        let hexdigest = format!("{digest:x}");
 
-        let hexdigest = format!("{:x}", digest);
-        let (dir, filename) = hexdigest.split_at(OBJ_DIR_LEN);
-        let path = self.location.join(dir);
-        let filepath = path.join(filename);
-        fs::create_dir(path);
-        fs::rename(uuid_filepath, filepath);
+        let (obj_dir_name, obj_file_name) = hexdigest.split_at(OBJ_DIR_LEN);
+        let obj_dir_path = self.location.join(obj_dir_name);
+        if !obj_dir_path.exists() {
+            fs::create_dir(&obj_dir_path)?;
+        }
+        let obj_file_path = obj_dir_path.join(obj_file_name);
+        fs::rename(uuid_file_path, obj_file_path)?;
 
         Ok(hexdigest)
     }
@@ -68,4 +73,8 @@ impl ObjectStore {
     
     // TODO: 
     // * pub(crate) fn write_tree(&self, directory: path::PathBuf) {}
+
+    // TODO: 
+    // * ?s are like todos themselves, need to revisit them and ensure best practice error-handling
+    //   and propogation
 }
